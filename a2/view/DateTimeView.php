78fd346d0a48a9875d8193d3bd27e83a133b02e1
Sing Trinh <dt222cc@student.lnu.se>
2015-09-04 23:40:04 +0000

    DateTimeView: Correct text.

--- a2/view/DateTimeView.php
+++ a2/view/DateTimeView.php
@@ -7,7 +7,7 @@ class DateTimeView {
 	
 	public function __construct() {
 		$this->timeZone = date_default_timezone_set('Europe/Stockholm');
-		$this->timeString = date('l, \t\h\e jS \o\f F Y, \t\h\e \t\i\m\e \i\s H:i:s');
+		$this->timeString = date('l, \t\h\e jS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
 	}
 	
 	public function show() {
