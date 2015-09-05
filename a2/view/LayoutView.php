96a2fd9a7f24a389e6c79776bbffb19a9cf9dbb9
Sing Trinh <dt222cc@student.lnu.se>
2015-09-05 00:01:26 +0000

    Title change

--- a2/view/LayoutView.php
+++ a2/view/LayoutView.php
@@ -7,7 +7,7 @@ class LayoutView {
       <html>
         <head>
           <meta charset="utf-8">
-          <title>Login Example</title>
+          <title>Login dt222cc</title>
         </head>
         <body>
           <h1>Assignment 2</h1>
