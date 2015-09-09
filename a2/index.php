68a21a8c71b82d1eb9a15e9056c7a4df29e9fa40
Sing Trinh <dt222cc@student.lnu.se>
2015-09-09 01:44:00 +0000

    Fixed missing text.

--- a2/index.php
+++ a2/index.php
@@ -24,6 +24,11 @@ if (!isset($_SESSION['IsLoggedIn'])) {
 
 $message = "";
 
+/**
+* If user just submitted a login form or pressed logout button
+* @param LoginView::UserName & LoginView::Password, Data from form
+* @return void, BUT writes the response message from the results
+*/
 if (isset($_POST["LoginView::Login"])) {
     if (empty($_POST["LoginView::UserName"])) {
         $message = "Username is missing";
